dabccc67aafda34460127d5c427d180a9c16a244
Julien Martin <julien314314@gmail.com>
2022-04-21 13:18:13 +0200

    change pinia syntaxe

--- pages/details-[name].vue
+++ pages/details-[name].vue
@@ -5,11 +5,12 @@
       <div class="absolute inset-0 bg-gradient-to-t from-white" />
     </div>
 
-    <div v-if="!errorComp" class="relative -mt-12 max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:px-8">
+    <div v-if="!errorFetchingApi" class="relative -mt-12 max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:px-8">
       <img class="absolute -top-40 left-0 right-0 ml-auto mr-auto w-48 h-48"
-        :src="mainStore.getSpriteUrl(pokemon.id.value) || ''" alt="" />
+        :src="mainStore.getSpriteUrl(pokemonInfos.id.value) || ''" alt="" />
 
       <div class="max-w-2xl mx-auto pt-20 text-center lg:max-w-4xl">
+        <AddToTeamBtn :pokemon="pokemon" />
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
         <p class="mt-4 text-gray-500">Each Pokémon belongs to a specific species but may take on a variant which makes
           it differ from other Pokémon of the same species,
@@ -18,7 +19,7 @@
 
       <dl
         class="mt-16 max-w-2xl mx-auto grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:max-w-none lg:grid-cols-3 lg:gap-x-8">
-        <div v-for="feature in pokemon" :key="feature.value" class="border-t border-gray-200 pt-4">
+        <div v-for="feature in pokemonInfos" :key="feature.value" class="border-t border-gray-200 pt-4">
           <dt class="font-medium text-gray-900">{{ feature.value }}</dt>
           <dd class="mt-2 text-sm text-gray-500">{{ feature.description }}</dd>
         </div>
@@ -28,36 +29,39 @@
     <div v-else class="relative -mt-12 max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:px-8">
       <img class="absolute -top-40 left-0 right-0 ml-auto mr-auto w-48 aspect-auto" src="~/assets/images/unown-question.png"
         alt="" />
-
       <div class="max-w-2xl mx-auto pt-20 text-center lg:max-w-4xl">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Pokemon doesn't exist</h2>
         <NuxtLink to="/">
           <p class="mt-4 text-gray-500">Go to Pokedex</p>
         </NuxtLink>
       </div>
-
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { useLayoutStore } from '~~/stores/layout';
 import { useMainStore } from '~~/stores/main';
+import AddToTeamBtn from '~~/components/buttons/AddToTeamBtn.vue';
+import { Pokemon } from  '~~/stores/main';
 const route = useRoute()
 const name = route.params.name
 const mainStore = useMainStore()
-const layoutStore = useLayoutStore()
 
 const { data: rawData, error } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
 
-const pokemon = {
-  name: { value: rawData.value?.name || '', description: 'The name for this resource.' },
+const pokemonInfos = {
   id: { value: rawData.value?.id || '', description: '	The identifier for this resource.' },
+  name: { value: rawData.value?.name || '', description: 'The name for this resource.' },
   base_experience: { value: rawData.value?.base_experience || '', description: 'The base experience gained for defeating this Pokémon.' },
   height: { value: rawData.value?.height || '', description: 'The height of this Pokémon in decimetres.' },
   order: { value: rawData.value?.order || '', description: 'Order for sorting. Almost national order, except families are grouped together.' },
   weight: { value: rawData.value?.weight || '', description: 'The weight of this Pokémon in hectograms.' },
 }
 
-const errorComp = computed(() => !!error.value)
+const pokemon: Pokemon = {
+  id: Number(pokemonInfos.id.value),
+  name: pokemonInfos.name.value,
+}
+
+const errorFetchingApi = computed(() => !!error.value)
 
 </script>
--- stores/main.ts
+++ stores/main.ts
@@ -6,86 +6,70 @@ export type Pokemon = {
   id: number,
   name: string,
   count?: number,
-  order?: number
-}
-type MainStore = {
-  offset: number,
-  team: Pokemon[],
-  modal: boolean
+  base_experience?: string,
+  height?: string,
+  order?: string,
+  weight?: string,
 }
 
-// export const useMainStore = defineStore('main', () => {
-//   const offset = ref(0)
-//   const team: Ref<Pokemon[]> = ref([])
-//   const modal = ref(false)
+export const useMainStore = defineStore('main', () => {
+  // STATE
+  const offset: Ref<Number> = ref(0)
+  const team: Ref<Pokemon[]> = ref([])
+  const modal: Ref<Boolean> = ref(false)
 
-//   const addToTeam = (pokemon: Pokemon) => {
-//     const layoutStore = useLayoutStore()
-//     layoutStore.resetAlerts()
+  // ACTIONS
+  const addToTeam = (pokemon: Pokemon) => {
+    const layoutStore = useLayoutStore()
+    layoutStore.resetAlerts()
 
-//     const pokemonInState = team.findIndex(p => p.id === pokemon.id)
-//     if (!isTeamFull) {
-//       if (pokemonInState === -1) {
-//         team.push({ ...pokemon, count: 1, order: team.length })
-//       }
-//       else {
-//         team[pokemonInState].count++
-//       }
-//       layoutStore.isSuccessAlertOpen = true
-//     }
-//     else {
-//       layoutStore.isErrorAlertOpen = true
-//     }
-//   } 
-// })
-
-export const useMainStore2 = defineStore('main', {
-  state: () => {
-    return {
-      offset: 0,
-      team: [],
-      modal: false
-    } as MainStore
-  },
-  actions: {
-    addToTeam(pokemon: Pokemon) {
-      const layoutStore = useLayoutStore()
-      layoutStore.resetAlerts()
-
-      const pokemonInState = this.team.findIndex(p => p.id === pokemon.id)
-      if (!this.isTeamFull) {
-        if (pokemonInState === -1) {
-          this.team.push({ ...pokemon, count: 1, order: this.team.length })
-        }
-        else {
-          this.team[pokemonInState].count++
-        }
-        layoutStore.isSuccessAlertOpen = true
+    const pokemonInState = team.value.findIndex(p => p.id === pokemon.id)
+    if (!isTeamFull.value) {
+      if (pokemonInState === -1) {
+        team.value.push({ ...pokemon, count: 1 })
       }
       else {
-        layoutStore.isErrorAlertOpen = true
+        team.value[pokemonInState].count = team.value[pokemonInState].count + 1
       }
-    },
-    removeFromTeam(pokemon: Pokemon) {
-      const pokemonInState = this.team.findIndex(p => p.id === pokemon.id)
-      if (pokemonInState !== -1) {
-        this.team[pokemonInState].count--
-        if (this.team[pokemonInState].count === 0) {
-          this.team.splice(pokemonInState, 1)
-        }
+      layoutStore.isSuccessAlertOpen = true
+    }
+    else {
+      layoutStore.isErrorAlertOpen = true
+    }
+  }
+
+  const removeFromTeam = (pokemon: Pokemon) => {
+    const pokemonInState = team.value.findIndex(p => p.id === pokemon.id)
+    if (pokemonInState !== -1) {
+      team.value[pokemonInState].count--
+      if (team.value[pokemonInState].count === 0) {
+        team.value.splice(pokemonInState, 1)
       }
     }
-  },
-  getters: {
-    getSpriteUrl() {
-      return (id: number) => id < 650 ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg` :
-        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-    },
-    getTotalCount(state) {
-      return state.team.reduce((acc, cur) => acc + (cur.count || 1), 0)
-    },
-    isTeamFull() {
-      return this.getTotalCount >= 6
-    }
+  }
+
+  const getSpriteUrl = (id: number) => {
+    return id < 650 ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg` :
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+  }
+
+  // GETTERS
+  const getTotalCount = computed(() => {
+    return team.value.reduce((acc, cur) => acc + (cur.count || 1), 0)
+  })
+
+  const isTeamFull = computed(() => {
+    return getTotalCount.value >= 6
+  })
+
+  return {
+    offset,
+    team,
+    modal,
+    addToTeam,
+    removeFromTeam,
+    getSpriteUrl,
+    getTotalCount,
+    isTeamFull
   }
 })
