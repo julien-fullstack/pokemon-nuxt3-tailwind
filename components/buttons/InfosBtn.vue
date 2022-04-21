332128ed25abf60ecbeacd203a22033568e4178f
Julien Martin <julien314314@gmail.com>
2022-04-22 01:13:11 +0200

    fix

--- components/buttons/InfosBtn.vue
+++ components/buttons/InfosBtn.vue
@@ -1,11 +1,11 @@
 <template>
-  <!-- <Popper :disabled="isDisabled" hover placement="left" disablePortal content="More infos" zIndex=9999> -->
+  <Popper :disabled="isDisabled" hover placement="left" disablePortal content="More infos" zIndex=9999>
     <span class="inline-flex items-center p-1 rounded-full text-white">
       <NuxtLink :to="`/details-${pokemon.name.toLowerCase()}`">
         <InformationCircleIconSolid class="h-8 w-8 fill-blue-600 hover:fill-blue-700" aria-hidden="true" />
       </NuxtLink>
     </span>
-  <!-- </Popper> -->
+  </Popper>
 </template>
 
 <script setup lang="ts">
